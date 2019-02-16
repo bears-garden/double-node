156d2fc5e12c4144049cc8975fe6cb129bb606fa
Arthur Anderson <arthur.anderson3@gmail.com>
2019-02-16 17:30:41 -0500

    updated

--- spec/double-node_spec.js
+++ spec/double-node_spec.js
@@ -13,154 +13,358 @@ describe("double-node", function() {
         expect( node.prev ).toEqual( null );
     });
 
-    it( "#insert null", function() {
-        let l = DNode.insert(null, 1);
+    it("#create_node", function(){
+        let node = DNode.create_node();
+        expect( node.next ).toEqual( null );
+        expect( node.data ).toEqual( null );
+        expect( node.prev ).toEqual( null );
+    });
 
-        expect( l.data ).toEqual( 1 );
-        expect( l.prev ).toEqual( null );
-        expect( l.next ).toEqual( null );
+    it( "#insert null", function() {
+        let head = DNode.insert(null, 1);
+
+        expect( head.data ).toEqual( 1 );
+        expect( head.prev ).toEqual( null );
+        expect( head.next ).toEqual( null );
+    });
+
+    it( "#insert_node null", function() {
+        let head = null;
+        let node = DNode.create_node(1);
+        head = DNode.insert_node(head, node);
+        expect( head ).toBe(node);
+        expect( head.data ).toEqual( 1 );
+        expect( head.prev ).toEqual( null );
+        expect( head.next ).toEqual( null );
     });
 
     it( "#insert 2", function() {
-        let l = DNode.insert(null, 1);
-        l = DNode.insert( l, 2 );
-        expect( l.data ).toEqual( 2 );
-        expect( l.prev ).toEqual( null );
-        expect( l.next.data ).toEqual( 1 );
-        expect( l.next.prev ).toEqual( l );
-        expect( l.next.next ).toEqual( null );
+        let head = DNode.insert(null, 1);
+        head = DNode.insert( head, 2 );
+        expect( head.data ).toEqual( 2 );
+        expect( head.prev ).toEqual( null );
+        expect( head.next.data ).toEqual( 1 );
+        expect( head.next.prev ).toEqual( head );
+        expect( head.next.next ).toEqual( null );
+    });
+
+    it( "#insert_node 2", function() {
+        let head = null;
+        let node = DNode.create_node(1);
+        head = DNode.insert_node(head, node);
+        let node2 = DNode.create_node(2);
+        head = DNode.insert_node( head, node2 );
+        expect( head ).toEqual( node2 );
+        expect( head.data ).toEqual( 2 );
+        expect( head.prev ).toEqual( null );
+        expect( head.next ).toEqual( node );
+        expect( head.next.data ).toEqual( 1 );
+        expect( head.next.prev ).toEqual( head );
+        expect( head.next.next ).toEqual( null );
     });
 
     it("#insert multiple", function() {
-        let l = null;
+        let head = null;
         for( let idx = 1; idx < 11; idx++ ){
-            l = DNode.insert( l, idx);
+            head = DNode.insert( head, idx);
         }
 
-        let n = l;
+        let n = head;
         for( let idx = 10; idx > 0; idx--, n = n.next ){
             expect( n.data ).toEqual( idx );
         }
     });
 
-    it( "#append null", function() {
-        let l = DNode.append(null, 1);
-
-        expect( l.data ).toEqual( 1 );
-        expect( l.prev ).toEqual( null );
-        expect( l.next ).toEqual( null );
-    });
-
-    it( "#append 2", function() {
-        let l = DNode.append(null, 1);
-        let n = DNode.append(l, 2 );
-
-        expect( l.data ).toEqual( 1 );
-        expect( l.prev ).toEqual( null );
-        expect( l.next.data ).toEqual( 2 );
-        expect( l.next.prev ).toEqual( l );
-        expect( l.next ).toEqual( n );
-        expect( n.prev ).toEqual( l );
-        expect( n.next ).toEqual( null );
-        expect( l.next.next ).toEqual( null );
-    });
-
-    it("#append multiple", function() {
-        let l = DNode.append( null, 0 );
-        let n = l;
+    it("#insert_node multiple", function() {
+        let head = null;
+        let node = null;
         for( let idx = 1; idx < 11; idx++ ){
-            n = DNode.append( n, idx);
+            node = DNode.create_node( idx );
+            head = DNode.insert_node( head, node);
         }
 
-        n = l;
-        for( let idx = 0; idx < 11; idx++, n = n.next ){
+        let n = head;
+        for( let idx = 10; idx > 0; idx--, n = n.next ){
             expect( n.data ).toEqual( idx );
         }
     });
 
-    it( "#find", function() {
-        let l = DNode.insert(null, 1);
-        l = DNode.insert(l, 2);
+    it( "#append null head", function() {
+        let head = DNode.append(null, 1);
 
-        let s = DNode.find(l, 1);
-        expect( s ).toEqual( l.next );
-        expect( s.data ).toEqual( 1 );
-        expect( s.prev).toEqual( l );
+        expect( head.data ).toEqual( 1 );
+        expect( head.prev ).toEqual( null );
+        expect( head.next ).toEqual( null );
+    });
 
-        let s2 = DNode.find( l, 2);
-        expect( s2 ).toEqual( l );
-        expect( s2.data ).toEqual( l.data );
+    it( "#append_node null head", function() {
+        let head = null;
+        let node = DNode.create_node(1);
+        head = DNode.append_node(head, node);
+
+        expect( head.data ).toEqual( node.data );
+        expect( head.prev ).toEqual( null );
+        expect( head.next ).toEqual( null );
+    });
+
+    it( "#append 2", function() {
+        let head = DNode.append(null, 1);
+        let tail = DNode.append(head, 2 );
+
+        expect( head.data ).toEqual( 1 );
+        expect( head.prev ).toEqual( null );
+        expect( head.next.data ).toEqual( 2 );
+        expect( head.next.prev ).toEqual( head );
+        expect( head.next ).toEqual( tail );
+        expect( tail.prev ).toEqual( head );
+        expect( tail.next ).toEqual( null );
+        expect( head.next.next ).toEqual( null );
+    });
+
+    it( "#append_node 2", function() {
+        let node = DNode.create_node(1);
+        let tail = DNode.append_node(null, node);
+        let node2 = DNode.create_node(2);
+        tail = DNode.append_node(tail, node2);
+
+        expect( tail.data ).toEqual( 2 );
+        expect( tail.next ).toEqual( null );
+        expect( tail.prev ).toEqual( node );
+        expect( tail.prev.data ).toEqual( 1 );
+        expect( tail.prev.next ).toEqual( tail );
+        expect( tail ).toEqual( node2 );
+        expect( node.next ).toEqual( tail );
+        expect( node.prev ).toEqual( null );
+        expect( node.next.next ).toEqual( null );
+    });
+    
+    it("#append multiple", function() {
+        let head = DNode.append( null, 0 );
+        let tail = head;
+        for( let idx = 1; idx < 11; idx++ ){
+            tail = DNode.append( tail, idx);
+        }
+
+        let cur = head;
+        for( let idx = 0; idx < 11; idx++, cur = cur.next ){
+            expect( cur.data ).toEqual( idx );
+        }
+    });
+    
+    it("#append_node multiple", function() {
+        let head = DNode.create_node(0);
+        let tail = head;
+        let app_node = null;
+        for( let idx = 1; idx < 11; idx++ ){
+            app_node = DNode.create_node(idx);
+            tail = DNode.append_node( tail, app_node);
+        }
+
+        let cur = head;
+        for( let idx = 0; idx < 11; idx++, cur = cur.next ){
+            expect( cur.data ).toEqual( idx );
+        }
+    });
+
+    it( "#find_node", function() {
+        let head = DNode.insert(null, 1);
+        head = DNode.insert(head, 2);
+
+        let found_node = DNode.find_node(head, 1);
+        expect( found_node ).toEqual( head.next );
+        expect( found_node.data ).toEqual( 1 );
+        expect( found_node.prev).toEqual( head );
+
+        let found_node2 = DNode.find_node( head, 2);
+        expect( found_node2 ).toEqual( head );
+        expect( found_node2.data ).toEqual( head.data );
+    });
+
+    function compareId( left, right ){
+        return left.id === right.id;
+    };
+
+    it( "#find_node {id:1}", function() {
+        let head = DNode.insert(null, {id:1});
+        head = DNode.insert(head, {id:2});
+
+        let found_node = DNode.find_node(head, {id:1}, compareId);
+        expect( found_node ).toEqual( head.next );
+        expect( found_node.data.id ).toEqual( 1 );
+        expect( found_node.prev).toEqual( head );
+
+        let found_node2 = DNode.find_node( head, {id:2}, compareId);
+        expect( found_node2 ).toEqual( head );
+        expect( found_node2.data.id ).toEqual( head.data.id );
     });
 
     it( "#predecessor", function() {
-        let l = DNode.insert(null, 1);
-        l = DNode.insert(l, 2);
-        l = DNode.insert(l, 3);
-        l = DNode.insert(l, 4);
+        let head = DNode.create_node(0);
+        for( let idx = 1; idx < 10; idx++ ){
+            head = DNode.insert(head, idx);
+        }
+        let pred = null;
+        for( let idx = 0; idx < 9; idx++ ){
+            pred = DNode.predecessor( head, idx);
+            expect( pred.data).toEqual(idx + 1);
+        }
+    });
 
-        let p = DNode.predecessor(l, 4);
-        expect(p).toEqual(null);
-        p = DNode.predecessor(l, 3);
-        expect(p.data).toEqual(4);
-        p = DNode.predecessor(l, 2);
-        expect(p.data).toEqual(3);
-        p = DNode.predecessor(l, 1);
-        expect(p.data).toEqual(2);
+    it( "#predecessor {id:1}", function() {
+        let len = 10;
+        let values = new Array(len);
+        for( let idx = 0; idx < len; idx++ ){
+            values[idx] = {id:idx};
+        }
+        let head = null;
+        for( let idx = 0; idx < len; idx++ ){
+            head = DNode.insert(head, values[idx] );
+        }
+
+        let p = null;
+        for( let idx = 0; idx < len - 1; idx++ ){
+            p = DNode.predecessor(head, values[idx], compareId );
+            expect(p.data.id).toEqual(values[ idx + 1 ].id);
+        }
+    });
+
+    it( "#predecessor_node", function() {
+        let head = DNode.create_node( 0 );
+        let node = null;
+        for( let idx = 1; idx < 10; idx++ ){
+            node = DNode.create_node( idx );
+            head = DNode.insert_node( head, node );
+        }
+        let found_node = null;
+        let pred = null;
+        for( let idx = 0; idx < 9; idx++ ){
+            found_node = DNode.find_node( head, idx );
+            expect( found_node instanceof DNode ).toBeTruthy();
+            pred = DNode.predecessor_node(head, found_node );
+            expect( pred.data ).toEqual( idx + 1);
+        }
     });
 
     it( "#remove", function(){
-        let n1 = new DNode({data:1});
-        let l = DNode.insert(n1, 2);
-        l = DNode.insert(l, 3 );
-        DNode.remove(l, 1);
-        let count = 0;
-        let node = 3;
-        for( let cur = l; cur !== null; cur = cur.next ){
-            expect( cur.data ).toEqual( node );
-            node--;
-            count++;
+        let head = DNode.create_node(1);
+        head = DNode.insert(head, 2);
+        head = DNode.insert(head, 3 );
+        // DNode.debugArray(head);
+        head = DNode.remove(head, 1);
+        let idx = 3;
+        for( let cur = head; cur !== null; cur = cur.next ){
+            expect( cur.data ).toEqual( idx-- );
         }
-        expect( count ).toEqual( 2 );
-        DNode.remove( l, 2 );
-        count = 0;
-        node = 3;
-        for( let cur = l; cur !== null; cur = cur.next ){
-            expect( cur.data).toEqual( node );
-            node--;
-            count++;
+        expect( DNode.size( head ) ).toEqual( 2 );
+        head = DNode.remove( head, 2 );
+        idx = 3;
+        for( let cur = head; cur !== null; cur = cur.next ){
+            expect( cur.data).toEqual( idx-- );
         }
-        expect( count).toEqual( 1 );
+        expect( DNode.size(head) ).toEqual( 1 );
+    });
+
+    it( "#remove_node", function(){
+        let head = DNode.create_node(1);
+        head = DNode.insert(head, 2);
+        head = DNode.insert(head, 3 );
+        let found = DNode.find_node( head, 1 );
+        head = DNode.remove_node(head, found);
+        let idx = 3;
+        for( let cur = head; cur !== null; cur = cur.next ){
+            expect( cur.data ).toEqual( idx-- );
+        }
+        expect( DNode.size( head ) ).toEqual( 2 );
+        found = DNode.find_node(head, 2);
+        head = DNode.remove_node(head, found );
+        idx = 3;
+        for( let cur = head; cur !== null; cur = cur.next ){
+            expect( cur.data).toEqual( idx-- );
+        }
+        expect( DNode.size(head) ).toEqual( 1 );
     });
 
     it( "#remove head", function(){
-        let l = DNode.insert(null, 1 );
-        l = DNode.insert(l, 2);
-        l = DNode.insert(l, 3 );
+        let head = DNode.create_node( 1 );
+        head = DNode.insert(head, 2);
+        head = DNode.insert(head, 3 );
+        // DNode.debugArray( head );
         for( let i = 3; i > 1; i-- ){
-            l = DNode.remove(l, i);
-            expect( l.data ).toEqual( i - 1 );
+            head = DNode.remove(head, i);
+            expect( head.data ).toEqual( i - 1 );
         }
-        l = DNode.remove(l, 1 );
-        expect( l ).toEqual( null );
+        head = DNode.remove(head, 1 );
+        expect( head ).toEqual( null );
+    });
+
+    it( "#remove_node head", function(){
+        let head = DNode.create_node( 1 );
+        head = DNode.insert(head, 2);
+        head = DNode.insert(head, 3 );
+        // DNode.debugArray( head );
+        let found = null;
+        for( let i = 3; i > 1; i-- ){
+            found = DNode.find_node(head, i );
+            head = DNode.remove_node(head, found);
+            expect( head.data ).toEqual( i - 1 );
+        }
+        found = DNode.find_node(head, 1);
+        head = DNode.remove_node(head, found);
+        expect( head ).toEqual( null );
     });
 
     it( "#remove tail", function(){
-        let l = DNode.append(null, 1 );
-        let t = DNode.append(l, 2);
-        t = DNode.append(t, 3 );
+        let head = DNode.append(null, 1 );
+        let tail = DNode.append(head, 2);
+        tail = DNode.append(tail, 3 );
         for( let i = 3; i > 1; i-- ){
-            t = DNode.remove(t, i);
-            expect( t.data ).toEqual( i - 1 );
+            tail = DNode.remove(tail, i);
+            expect( tail.data ).toEqual( i - 1 );
         }
-        l = DNode.remove(l, 1 );
-        expect( l ).toEqual( null );
+        head = DNode.remove(head, 1 );
+        expect( head ).toEqual( null );
     });
 
+    it( "#remove_node tail", function(){
+        let head = DNode.append(null, 1 );
+        let tail = DNode.append(head, 2);
+        tail = DNode.append(tail, 3 );
+        let found = null;
+        for( let i = 3; i > 1; i-- ){
+            found = DNode.find_node( tail, i );
+            tail = DNode.remove_node(tail, found);
+            expect( tail.data ).toEqual( i - 1 );
+        }
+        expect( tail ).toEqual( head );
+        found = DNode.find_node(head, 1);
+        head = DNode.remove_node(head, found );
+        expect( head ).toEqual( null );
+    });
+
+    it( "#remove middle", function(){
+        let head = DNode.create_node( 1 );
+        let tail = DNode.append(head, 2);
+        tail = DNode.append(tail, 3 );
+        head = DNode.remove(head, 2 );
+        expect( DNode.size( head) ).toEqual(2);
+    });
+
+    it( "#remove_node middle", function(){
+        let head = DNode.create_node( 1 );
+        let tail = head;
+        for( let idx = 2; idx < 11; idx++ ){
+            tail = DNode.append( tail, idx);
+        }
+        let found = DNode.find_node( head, 5 );
+        head = DNode.remove_node(head, found );
+        expect( DNode.size( head) ).toEqual(9);
+    });
+    
     it( "#size", function(){
-        let n1 = new DNode({data:1});
-        let l = DNode.insert(n1, 2);
-        expect( DNode.size( l )).toEqual( 2 );
-        l = DNode.insert(l, 3 );
-        expect( DNode.size( l ) ).toEqual( 3 );
+        let head = DNode.create_node(1);
+        head = DNode.insert(head, 2);
+        expect( DNode.size( head )).toEqual( 2 );
+        head = DNode.insert(head, 3 );
+        expect( DNode.size( head ) ).toEqual( 3 );
     });
 })
--- src/double-node.js
+++ src/double-node.js
@@ -1,8 +1,17 @@
 /**
  * Created by arthuranderson on 3/12/17.
  */
-const defaults = require('lodash.defaults');
+const defaults = require("lodash.defaults");
+const isFunction = require("lodash.isfunction");
 
+/**
+ * Default Comparer
+ * @param l
+ * @param r
+ * @returns {boolean}
+ */
+
+function defComparer( l, r ){ return l === r; }
 class DoubleNode{
     constructor( obj ){
         let defObj = { next: null, prev: null, data:null};
@@ -12,77 +21,203 @@ class DoubleNode{
         this.data = obj.data;
     };
 
-    static find(l, obj ){
-        if( l === null ){
+    /**
+     * create_node - A Factory method for creating DoubleNode
+     * @param obj - is the data that will be stored, this node is not linked
+     * @returns {DoubleNode} a new double node
+     */
+    static create_node( obj ){
+        return new DoubleNode({data:obj});
+    }
+
+    /**
+     * find_node - helper method to find an object using a comparer
+     * @param head - a node to start the find from
+     * @param obj - contains data to find the node
+     * @param comparer - [optional] default method( right, left ) => { return right === left; }
+     * @returns {DoubleNode}
+     */
+    static find_node(head, obj, comparer ){
+        comparer = isFunction( comparer ) ? comparer : defComparer;
+        if( head === null ){
             return null;
         }
-        if( l.data === obj ){
-            return l;
+        let cur = head;
+        while( cur !== null ){
+            if( comparer( obj, cur.data ) ){
+                break;
+            }
+            cur = cur.next;
+        }
+        return cur;
+    };
+
+    /**
+     * insert - helper to create a new node with the obj data and prepends the node
+     * @param head - starting node to prepend
+     * @param obj - data to store
+     * @returns {DoubleNode}
+     */
+    static insert(head, obj ){
+        let node = DoubleNode.create_node( obj );
+        return DoubleNode.insert_node( head, node);
+    };
+
+    /**
+     * insert_node - prepend a node to the head
+     * @param head - {DoubleNode} to prepended
+     * @param node - {DoubleNode} to insert - assuming a new node
+     * @returns {DoubleNode} node
+     */
+    static insert_node( head, node ){
+        if( !(node instanceof DoubleNode) ){
+            throw new TypeError("expecting node to be DoubleNode");
+        }
+        node.next = head;
+        if( head !== null) {
+            if (head.prev === null) {
+                head.prev = node;
+            } else {
+                head.prev.next = node;
+                head.prev = node;
+            }
+        }
+        return node;
+    }
+
+    /**
+     * append -  creates a new node with obj as data and appends cur_node
+     * @param cur_node - {DoubleNode} appends obj new node
+     * @param obj - data to create new {DoubleNode} with
+     * @returns {DoubleNode}
+     */
+    static append(cur_node, obj ){
+        if( cur_node === null ){
+            return DoubleNode.create_node( obj );
         }else{
-            return DoubleNode.find( l.next, obj );
+            let node = DoubleNode.create_node( obj );
+            return DoubleNode.append_node(cur_node, node );
         }
     };
 
-    static insert(n, obj ){
-        let node = new DoubleNode( { data: obj, next: n } );
-        if( n !== null) {
-            if (n.prev === null) {
-                n.prev = node;
-            } else {
-                n.prev.next = node;
-                n.prev = node;
-            }
+    /**
+     * append_node - helper to update links when appending a node
+     * @param cur_node - {DoubleNode}
+     * @param node - {DoubleNode}
+     * @returns {DoubleNode} node
+     */
+    static append_node(cur_node, node ){
+        if( !(node instanceof DoubleNode) ){
+            throw new TypeError("expecting node to be DoubleNode");
+        }
+        if( cur_node === null ){
+            return node;
+        }
+        node.next = cur_node.next;
+        node.prev = cur_node;
+        cur_node.next = node;
+        if( node.next !== null ) {
+            node.next.prev = node;
         }
         return node;
     };
 
-    static append(n, obj ){
-        if( n === null ){
-            return new DoubleNode( { data: obj } );
-        }else{
-            let node = new DoubleNode({prev:n, data:obj, next: n.next} );
-            n.next = node;
-            if( node.next !== null ) {
-                node.next.prev = node;
-            }
-            return node;
-        }
-    }
-
-    static predecessor( n, obj ){
-        let next = DoubleNode.find(n,obj);
-        return next.prev;
+    /**
+     * predecessor - finds the node from head and returns its predecessor
+     * @param head
+     * @param obj
+     * @param comparer -
+     * @returns {DoubleNode}
+     */
+    static predecessor( head, obj, comparer ){
+        let node = DoubleNode.find_node(head,obj, comparer);
+        return DoubleNode.predecessor_node( head, node );
     };
 
-    static remove(l, obj ){
-        let p = DoubleNode.find( l, obj );
-        if( p !== null ){
-            // removing the node we found!
-            if( p === l) {
-                if (p.prev === null) {
-                    l = p.next;
-                }else if( p.next === null ){
-                    l = p.prev;
-                }
-            }
-            // p next and prev needs to be relinked
-            if( p.prev !== null ){
-                p.prev.next = p.next;
-            }
-            if( p.next !== null ){
-                p.next.prev = p.prev;
-            }
+    /**
+     * predecessor_node - returns the nodes predecessor ( same signature as single-node )
+     * @param head
+     * @param node
+     * @returns {DoubleNode}
+     */
+    static predecessor_node( head, node ){
+        if( node === null ){
+            return null;
         }
-        return l;
+        return node.prev;
     };
 
-    static size( l ){
+    /**
+     * remove - finds a node with obj's data using [optional] comparer or default comparer
+     * @param cur_node - {DoubleNode} start the find from here
+     * @param obj - obj data to find the node
+     * @param comparer - [optional] customer comparer ( l, r ) => { l === r; }
+     * @returns {DoubleNode}
+     */
+    static remove(cur_node, obj, comparer ){
+        let node = DoubleNode.find_node( cur_node, obj, comparer );
+        return DoubleNode.remove_node( cur_node, node );
+    };
+
+    /**
+     * remove_node - manages the relinking process with cur_node
+     * @param cur_node - {DoubleNode}
+     * @param node - {DoubleNode}
+     * @returns {DoubleNode}
+     */
+    static remove_node( cur_node, node ){
+        if( node === null ) {
+            return cur_node;
+        }
+        // removing the node we found!
+        if( node === cur_node) {
+            if (node.prev === null) {
+                cur_node = node.next;
+            }else if( node.next === null ){
+                cur_node = node.prev;
+            }
+        }
+        if( node.prev !== null ){
+            node.prev.next = node.next;
+        }
+        if( node.next !== null ){
+            node.next.prev = node.prev;
+        }
+        return cur_node;
+    };
+
+    /**
+     * size - returns the number of nodes linked together from head
+     * @param head
+     * @returns {number}
+     */
+    static size( head ){
         let count = 0;
-        for( let cur = l; cur !== null; cur = cur.next ){
+        for( let cur = head; cur !== null; cur = cur.next ){
             count++;
         }
         return count;
     };
+
+    /**
+     * toArray - creates an Array from the list's data
+     * @param head - {DoubleNode} start of the list to
+     * @returns {Array}
+     */
+    static toArray(head){
+        let len = DoubleNode.size(head);
+        let arr = new Array(len);
+        for( let cur = head, idx = 0; cur !== null; cur = cur.next ){
+            arr[idx++] = cur.data;
+        }
+        return arr;
+    }
+
+    static debugArray( head ){
+        for( let cur = head, idx = 0; cur !== null; cur = cur.next ){
+            console.log( "[ " + idx++ + ", " + JSON.stringify( cur.data) + " ]" );
+        }
+    }
 };
 
 module.exports = DoubleNode;
